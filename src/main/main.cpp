1074c584b55ec2b4464b890ed83a397763d92874
Mika Urtela <mika.urtela@soulaimstudios.com>
2012-02-14 23:56:19 +0200

    ..

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "graphics/opengl.h"
 #include "graphics/window.h"
+#include "graphics/texturehandler.h"
+
+#include "misc/timer.h"
 
 int main(int argc, char** argv) {
 
@@ -9,8 +12,13 @@ int main(int argc, char** argv) {
 	window.createWindow(600, 400);
 
 	OpenGL gl;
+	
+	TextureHandler::getSingleton().createTexture("test", "data/img.png");
+	TextureHandler::getSingleton().deleteTexture("test");
 
-	while(1) {
+	long long startTime = Timer::time_now();
+
+	while(Timer::time_now() - startTime < 5000) {
 		window.swap_buffers();
 	}
 
